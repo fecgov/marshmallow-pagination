4bd0d85f827f70f3448f296e4caf2d2607c3490d
tmpayton <121631201+tmpayton@users.noreply.github.com>
2023-12-28 14:52:46 -0500

    Squashed commit of the following:
    commit 30a40d31bb71985b41a3dbc03473627520ff59d4
    Merge: b6805b7 fc060c9
    Author: Priya Kasireddy <pkasireddy@fec.gov>
    Date:   Mon Jul 31 18:15:54 2023 -0400
    
        Merge pull request #14 from fecgov/upgrade-marshmallow-v3
    
        upgrade marshmallow-v3
    
    commit fc060c95b2e486a20052cb64b1d8580cb08e0b75
    Author: tmpayton <121631201+tmpayton@users.noreply.github.com>
    Date:   Mon Jul 31 18:14:08 2023 -0400
    
        upgrade marshmallow-v3
    
    commit b6805b7db8f64a3bbcb952db6a8521308ee52042
    Merge: d4d363c 003d6f4
    Author: Priya Kasireddy <pkasireddy@fec.gov>
    Date:   Mon Jul 31 18:11:31 2023 -0400
    
        Merge pull request #13 from fecgov/feature/upgrade-marshmallow-v3
    
        upgrade marshmallow v3
    
    commit 003d6f43ead3791de096b668f51c2f0d620c7dc5
    Author: tmpayton <121631201+tmpayton@users.noreply.github.com>
    Date:   Mon Jul 31 18:09:17 2023 -0400
    
        upgrade marshmallow v3
    
    commit d4d363c5d2e13868d7601452edb4efa46851fb83
    Merge: 1db0147 a77faac
    Author: Corinne Lucas <66386084+cnlucas@users.noreply.github.com>
    Date:   Mon Jul 31 17:58:13 2023 -0400
    
        Merge pull request #11 from fecgov/feature/upgrade-marshmallow
    
         upgrade to marshmallow v3
    
    commit a77faacc48e73e1dd89b9c2ba8522ce60e423f59
    Author: tmpayton <121631201+tmpayton@users.noreply.github.com>
    Date:   Thu Jul 20 14:32:16 2023 -0400
    
        upgrade to marshmallow v3
    
    commit 1db01472e960b655824b2e741ea574c805f3154e
    Merge: 0f4a530 c963871
    Author: tmpayton <121631201+tmpayton@users.noreply.github.com>
    Date:   Mon Jul 17 11:54:45 2023 -0400
    
        Merge pull request #9 from fecgov/revert-8-develop
    
        Revert "Marshmallow 3 changes, release branch"
    
    commit c9638714089fc6986d4f1ca6a1b64b9cb9b72a6b
    Author: Corinne Lucas <66386084+cnlucas@users.noreply.github.com>
    Date:   Mon Jul 17 11:51:05 2023 -0400
    
        Revert "Marshmallow 3 changes, release branch"
    
    commit 0f4a530f9e8c50c40f375e06f72d74776115b4bf
    Merge: 3180169 2c9f1c5
    Author: Priya Kasireddy <pkasireddy@fec.gov>
    Date:   Mon Jul 17 09:43:13 2023 -0400
    
        Merge pull request #8 from fecgov/develop
    
        Marshmallow 3 changes, release branch
    
    commit 3180169f93680ee5e07baf97d28d0913d9a3f036
    Merge: 73fe389 b778939
    Author: Priya Kasireddy <pkasireddy@fec.gov>
    Date:   Wed Nov 6 09:55:29 2019 -0500
    
        Merge pull request #4 from fecgov/develop
    
        merge develop-->master

--- marshmallow_pagination/schemas.py
+++ marshmallow_pagination/schemas.py
@@ -4,8 +4,8 @@ import six
 import marshmallow as ma
 
 class PageSchemaOpts(ma.schema.SchemaOpts):
-    def __init__(self, meta):
-        super(PageSchemaOpts, self).__init__(meta)
+    def __init__(self, meta, ordered=False):
+        super(PageSchemaOpts, self).__init__(meta, ordered=ordered)
         self.results_schema_class = getattr(meta, 'results_schema_class', None)
         self.results_field_name = getattr(meta, 'results_field_name', 'results')
         self.results_schema_options = getattr(meta, 'results_schema_options', {})
